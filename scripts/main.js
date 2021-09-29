762598260b7b0f4faeeaa5b0db56c9c3b2b8a9d9
farias <fariasjunior96@gmail.com>
2021-09-29 19:20:18 -0300

    nav styling and routing

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
+function loadContent(pageRef) {
+    $.ajax({
+        url: pageRef,
+        data: {},
+        type: 'GET',
+        dataType: 'text',
+        success: (response) => {
+            $('#page-body').html(response)
+        },
+        error: (error) => {
+            console.log('error: ' + error.statusText)
+            console.log(error)
+        },
+        complete: (xhr, status) => {}
+    })
+}
+
 $(document).ready(function() {
 
+})
+
+$(document).on('click', '#current-button', e => {
+    e.preventDefault()
+    loadContent('./sections')
 })
