73f7b758d68f1d06f6d2eb82c563825ab1380db9
farias <fariasjunior96@gmail.com>
2021-09-30 08:48:59 -0300

    rethink inclusion inputs behaviour logic

--- scripts/inclusion.js
+++ scripts/inclusion.js
@@ -6,28 +6,43 @@ function newLacoInput(number) {
     return "<input type=\"text\" class=\"form-control\" id=\"inclusion-laco-" + number + "\">"
 }
 
+function checkForChildElement(parent, children) {
+    for (var element of $('#' + parent).children()) {
+        if (element.id == children) return true
+    }
+    return false
+}
 
 function createNextPeInput(number) {
     $('#inclusion-pe').append(newPeInput(number))
-    $(document).one('input', '#inclusion-pe-' + number, e => {
-        createNextPeInput(number + 1)
-    })
+    setPeInputBehaviour(number)
 }
 
 function createNextLacoInput(number) {
     $('#inclusion-laco').append(newLacoInput(number))
-    $(document).one('input', '#inclusion-laco-' + number, e => {
-        createNextLacoInput(number + 1)
+    setLacoInputBehaviour(number)
+}
+
+function setPeInputBehaviour(number) {
+    $(document).on('input', '#inclusion-pe-' + number, e => {
+        var next = number + 1
+        if (!checkForChildElement('inclusion-pe', 'inclusion-pe-' + next)) { // terrible performance, there's gotta be a better way
+            createNextPeInput(next)
+        }
     })
 }
 
-$(document).one('input', '#inclusion-pe-1', e => {
-    createNextPeInput(2)
-})
+function setLacoInputBehaviour(number) {
+    $(document).on('input', '#inclusion-laco-' + number, e => {
+        var next = number + 1
+        if (!checkForChildElement('inclusion-laco', 'inclusion-laco-' + next)) {
+            createNextLacoInput(next)
+        }
+    })
+}
 
-$(document).one('input', '#inclusion-laco-1', e => {
-    createNextLacoInput(2)
-})
+setPeInputBehaviour(1)
+setLacoInputBehaviour(1)
 
 function getValuesFromChildrenInputs(element) {
     // for some reason the code below isn't working
