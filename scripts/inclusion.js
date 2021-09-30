48ee6fe486479ea85fbb5a754c61e6325673807e
farias <fariasjunior96@gmail.com>
2021-09-29 21:25:59 -0300

    inclusion page working

--- scripts/inclusion.js
+++ scripts/inclusion.js
@@ -1,13 +1,55 @@
-function addListener(number) {
-    $(document).one('click', '#inclusion-pe-' + number, e => {
-        var inputNumber = e.target.id.substring(e.target.id.length - 1)
-        console.log('ok')
-        $('#inclusion-pe').append("<input type=\"text\" class=\"form-control\" id=\"inclusion-pe-" + inputNumber + "\">")
+function newPeInput(number) {
+    return "<input type=\"text\" class=\"form-control\" id=\"inclusion-pe-" + number + "\">";
+}
+
+function newLacoInput(number) {
+    return "<input type=\"text\" class=\"form-control\" id=\"inclusion-laco-" + number + "\">"
+}
+
+
+function createNextPeInput(number) {
+    $('#inclusion-pe').append(newPeInput(number))
+    $(document).one('input', '#inclusion-pe-' + number, e => {
+        createNextPeInput(number + 1)
     })
 }
 
-addListener(1)
+function createNextLacoInput(number) {
+    $('#inclusion-laco').append(newLacoInput(number))
+    $(document).one('input', '#inclusion-laco-' + number, e => {
+        createNextLacoInput(number + 1)
+    })
+}
+
+$(document).one('input', '#inclusion-pe-1', e => {
+    createNextPeInput(2)
+})
+
+$(document).one('input', '#inclusion-laco-1', e => {
+    createNextLacoInput(2)
+})
+
+function getValuesFromChildrenInputs(element) {
+    // for some reason the code below isn't working
+    // return element.children()
+    //.filter(c => c.tagName == 'INPUT')
+
+    var arr = []
+
+    for (e of element.children()) {
+        if (e.tagName === 'INPUT') {
+            var val = document.getElementById(e.id).value
+            if (val.length > 0) arr.push(val)
+        }
+    }
+
+    return arr
+}
 
 $(document).on('click', '#inclusion-start-button', e => {
-    console.log('start')
+    var peList = getValuesFromChildrenInputs($('#inclusion-pe'))
+    var lacoList = getValuesFromChildrenInputs($('#inclusion-laco'))
+
+    console.log(peList)
+    console.log(lacoList)
 })
