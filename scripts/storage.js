7eec07b0ee39dfc866531a0f266a16d076c9a7de
farias <fariasjunior96@gmail.com>
2021-09-30 10:09:08 -0300

    session saving now with update

--- scripts/storage.js
+++ scripts/storage.js
@@ -21,8 +21,22 @@ class Storage {
 
     static saveSession(session) {
         var sessionList = this.getAllSessions()
-        sessionList.push(session)
-        sessionList = this.sortSessionList(sessionList)
+
+        var alreadyPresentIndex = null
+        for (var i in sessionList) {
+            if (sessionList[i].timestamp === session.timestamp) {
+                alreadyPresentIndex = i
+                break
+            }
+        }
+
+        if (alreadyPresentIndex) {
+            sessionList[alreadyPresentIndex] = session
+        } else {
+            sessionList.push(session)
+            sessionList = this.sortSessionList(sessionList)
+        }
+
         window.localStorage.setItem(this.sessionListKey, JSON.stringify(sessionList))
     }
 }
