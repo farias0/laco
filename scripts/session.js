70fa08c7c199638095369dcaba8e3d1dbf80052a
farias <fariasjunior96@gmail.com>
2021-09-30 23:07:51 -0300

    barebones match time input

--- scripts/session.js
+++ scripts/session.js
@@ -1,5 +1,67 @@
+function createTimeInput(set, match) {
+    var buttonId = 'sat-btn-' + set + '-' + match
+    var timeInputId = 'time-' + set + '-' + match
+
+    var div = document.createElement('div')
+        // div.classList.add('input-group')
+        // div.classList.add('input-group-lg')
+
+    var timeInput = document.createElement('input')
+    timeInput.classList.add('form-control')
+    timeInput.id = timeInputId
+        /* timeInput.type = 'time'
+        timeInput.step = '1'
+        timeInput.min = '00:00:00'
+        timeInput.max = '20:00:00' */
+    div.appendChild(timeInput)
+
+    var buttonDiv = document.createElement('div')
+        // buttonDiv.classList.add('input-group-append')
+
+    /* var button = document.createElement('input')
+    button.type = 'checkbox'
+    button.classList.add('btn-check')
+    button.id = buttonId
+    button.autocomplete = 'off'
+
+    var buttonLabel = document.createElement('label')
+    buttonLabel.classList.add('btn')
+    buttonLabel.classList.add('btn-primary')
+    buttonLabel.classList.add('btn-danger')
+    buttonLabel.classList.add('small')
+    buttonLabel.htmlFor = buttonId
+    buttonLabel.textContent = 'SAT' 
+    
+    buttonDiv.appendChild(button)
+    buttonDiv.appendChild(buttonLabel) */
+
+    var buttonLabel = document.createElement('label')
+
+    var button = document.createElement('input')
+    button.type = 'checkbox'
+    button.id = buttonId
+
+    buttonLabel.textContent = 'SAT'
+
+    $(button).change(() => {
+        if ($(button).is(':checked')) {
+            $(timeInput).prop('disabled', true);
+        } else {
+            $(timeInput).prop('disabled', false);
+        }
+    });
+
+    buttonLabel.appendChild(button)
+    buttonDiv.appendChild(buttonLabel)
+
+    div.appendChild(buttonDiv)
+
+    return div
+}
+
 function createTh(label) {
     var th = document.createElement('th')
+    th.classList.add('match-table-column')
     th.scope = 'col'
     th.innerText = label
     return th
@@ -11,7 +73,7 @@ function createTd(label) {
     return td
 }
 
-function createSetTable(set) {
+function createSetTable(setNumber, matches) {
     var table = document.createElement('table')
     table.id = 'match-set-table'
     table.classList.add('table')
@@ -24,11 +86,11 @@ function createSetTable(set) {
     thead.appendChild(headerTr)
 
     var tbody = table.createTBody()
-    for (var match in set) {
+    for (var match in matches) {
         var tr = document.createElement('tr')
-        tr.appendChild(createTd(set[match].getCabeceiro()))
-        tr.appendChild(createTd(set[match].getPeseiro()))
-        tr.appendChild(createTd(set[match].getTime()))
+        tr.appendChild(createTd(matches[match].getCabeceiro()))
+        tr.appendChild(createTd(matches[match].getPeseiro()))
+        tr.appendChild(createTimeInput(setNumber, match))
         tbody.appendChild(tr)
     }
 
@@ -36,7 +98,8 @@ function createSetTable(set) {
 }
 
 function loadSession(session) {
-    for (var set of session.getMatches()) {
-        createSetTable(set)
+    var sets = session.getMatches()
+    for (var setNumber in sets) {
+        createSetTable(setNumber, sets[setNumber])
     }
 }
