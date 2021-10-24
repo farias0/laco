142460e939d48e489121bb0c9b7d0dd124e4359e
farias <fariasjunior96@gmail.com>
2021-10-24 16:26:04 -0300

    phase 2 screen results saving

--- scripts/session-p2.js
+++ scripts/session-p2.js
@@ -100,6 +100,22 @@ class Session2Screen {
         return table
     }
 
+    static createToResultsButton() { //
+        const button = document.createElement('button')
+        button.id = 'to-session-results-button' //
+        button.classList.add('btn', 'btn-success', 'btn-lg', 'float-right') //
+        button.innerHTML = 'Terminar' //
+
+        $(document).on('click', '#to-session-results-button', e => { //
+            this.setMatchesResults()
+            Storage.saveSession(this.session)
+            // loadContent('./sections/session-p2.html', () => Session2Screen.loadSessionP2(this.session))
+            console.log(this.session)
+        })
+
+        return button
+    }
+
     static loadSessionP2(session) {
         this.session = session
 
@@ -108,5 +124,38 @@ class Session2Screen {
         qualified.sort((a, b) => Match.timeStringTreatment(b.timeP1) - Match.timeStringTreatment(a.timeP1))
 
         $('#sessionP2').append(this.createSetTable(1, qualified))
+
+        $('#sessionP2').append(this.createToResultsButton())
+    }
+
+    // returns a map (key=cabeceiro) with submaps (key=peseiro) with the result as a value (time or SAT)
+    static getResultsAsMap() {
+        const matches = {}
+        for (const table of $('#sessionP2').children()) { //
+            if (table.id === 'match-set-table') {
+                const rows = table.tBodies.item(0).rows
+                for (const row of rows) {
+                    const players = row.childNodes[1].innerText.split(' x ')
+                    const c = players[0]
+                    const p = players[1]
+                    const result = this.getValuesFromTimeInput(row.childNodes[3])
+                    if (!matches[c]) matches[c] = {}
+                    matches[c][p] = result.isSat ? Match.getSatValue() : result.time
+                } 
+            }
+        }
+        return matches
+    }
+
+    // retrieve and set the result of each match to the session object
+    static setMatchesResults() {
+        const matches = this.session.getMatches()
+        const results = this.getResultsAsMap()
+
+        for (const match of matches) {
+            if (match.timeP1 !== Match.getSatValue) {
+                match.timeP2 = results[match.cabeceiro][match.peseiro] //
+            }
+        }
     }
 }
